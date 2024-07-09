5e41b32d1a4efeb4b759034be9e9917d98635392
Sublari Leela Deepak <leeladeepak123@gmail.com>
2024-07-09 17:37:18 +0530

    login changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
-    
+    {
+        path: '',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./pages/register/register.component').then((component) => component.RegisterComponent)
+    }
 ];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule,MatInputModule,MatFormFieldModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  
 
 }
