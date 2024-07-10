1d424165d262a1b33da7190f567a016e8887e395
Sublari Leela Deepak <leeladeepak123@gmail.com>
2024-07-10 15:22:55 +0530

    UI changes commit

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,20 +1,49 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule,MatInputModule,MatFormFieldModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule,MatInputModule,MatFormFieldModule,ReactiveFormsModule,RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  loginForm!:FormGroup;
+
+  constructor(private fb:FormBuilder){}
+
+  ngOnInit(){
+    this.loginForm = this.fb.group({
+      email:['',Validators.required],
+      password:['',Validators.required]
+    })
+  }
+  
+  onSubmit(){
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value);
+      if(this.loginForm.value['email']!="" && this.loginForm.value["password"]!=""){
+        alert("Login Successful");
+      }
+    }else{
+      alert("Invalid Login");
+    }
+    this.loginForm.reset();
+  }
+
+ 
+    
+}
+
   
 
-}
+
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { RouterLink } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule,MatFormFieldModule,MatInputModule,MatInputModule,MatButtonModule,MatIconModule,RouterLink,ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  registerForm!: FormGroup;
+  selectedGender: string = '';
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(){
+    this.registerForm = this.fb.group({
+      email: ['', [ Validators.email]],
+      password: ['', [Validators.minLength(6)]],
+      gender: [''] 
+    });
+  }
+
+  onSubmit(){
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      if(this.registerForm.value['email']!="" && this.registerForm.value["password"]!="" && this.registerForm.value['gender']!=""){
+        alert("Register Successful");
+      }
+    } else {
+      alert("Invalid Data");
+    }
+    this.registerForm.reset();
+    
+  }
+
+  
+
+  
 
 }
